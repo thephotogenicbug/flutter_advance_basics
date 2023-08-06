2dec21f06a078299c28dabcf98e661a6ea96f985
thephotogenicbug <navinkumarachari@gmail.com>
2023-08-06 20:16:34 +0530

    Design Start Screen

---
+++ lib/start_screen.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+
+class StartScreen extends StatelessWidget {
+  const StartScreen({super.key});
+
+  @override
+  Widget build(context) {
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Image.asset(
+            'assets/images/quiz-logo.png',
+            width: 300,
+            color: const Color.fromARGB(150, 255, 255, 255),
+          ),
+          const SizedBox(
+            height: 80,
+          ),
+          const Text(
+            "Learn flutter the fun way!",
+            style: TextStyle(color: Colors.white, fontSize: 24),
+          ),
+          const SizedBox(height: 30),
+          OutlinedButton.icon(
+            onPressed: () {},
+            style: OutlinedButton.styleFrom(
+              foregroundColor: Colors.white,
+            ),
+            icon: const Icon(Icons.arrow_right_alt),
+            label: const Text("Start Quiz"),
+          )
+        ],
+      ),
+    );
+  }
+}
